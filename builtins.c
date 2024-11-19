e4088c9a2ff1cae667825e8a19549b425a0fe388
Imane Qandyl <imqandyl@lab2r4s14.42abudhabi.ae>
2024-11-19 08:32:27 +0400

    update

---
+++ builtins.c
@@ -0,0 +1,8 @@
+// // Study how to implement
+// cd    // Directory navigation
+// echo  // Output text
+// env   // Environment variables
+// exit  // Shell termination
+// export// Set environment variables
+// unset // Remove environment variables
+// pwd   // Print working directory

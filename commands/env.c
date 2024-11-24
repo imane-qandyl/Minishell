483c116261527f945f0b16b1b8e52f09243867b3
istepano <istepano@student.42abudhabi.ae>
2024-11-24 18:23:12 +0400

    added commands

---
+++ commands/env.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iliastepanov <iliastepanov@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/17 14:04:57 by iliastepano       #+#    #+#             */
+/*   Updated: 2024/11/24 17:19:46 by iliastepano      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+
+void custom_env()
+{
+    int i;
+
+    i = -1;
+    while (g_environ[0][++i] != NULL)
+        printf("%s=%s\n", g_environ[0][i], g_environ[1][i]);
+}
+
+// int main()
+// {
+//     init_environ();
+//     custom_env();
+//     cleanup_environ();
+//     return (0);
+// }
+

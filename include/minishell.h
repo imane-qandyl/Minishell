c05c232843defa79f7a52a6e99c36a3322cd8f35
Imane Qandyl <imqandyl@lab2r4s14.42abudhabi.ae>
2024-12-01 19:43:06 +0400

    update

--- include/minishell.h
+++ include/minishell.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -7,11 +6,9 @@
 /*   By: imqandyl <imqandyl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 21:15:25 by imqandyl          #+#    #+#             */
-/*   Updated: 2024/11/30 23:57:19 by imqandyl         ###   ########.fr       */
+/*   Updated: 2024/12/01 19:42:47 by imqandyl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-=======
->>>>>>> 6c2e1f9f830287623e796f986a7e976da6d8ddbc
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -113,15 +110,11 @@ int handle_quotes(char *input, int *i, char *buffer, int *j);
 void free_command_list(t_command *cmd_list);
 void run_parser_tests(void);
 
-<<<<<<< HEAD
 void handle_sigint(int sig);
 void handle_sigquit(int sig);
 void handle_sigterm(int sig);
 
 char *expand_env_vars(char *input, int *exit_status);
-
-=======
-
 
 void custom_pwd(void);
 void custom_env(void);
@@ -129,5 +122,5 @@ void custom_pwd(void);
 
 void init_environ(void);
 void cleanup_environ(void);
->>>>>>> 6c2e1f9f830287623e796f986a7e976da6d8ddbc
+
 #endif
